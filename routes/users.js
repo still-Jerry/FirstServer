a5ee7b1690261d8b850179bb4e7266acca49a079
still_Jerry <oljaapelsin@gmail.com>
2022-07-06 22:14:20 +0300

    structured code

--- routes/users.js
+++ routes/users.js
@@ -2,36 +2,38 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+var FileName='testFile.txt'
+
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/test', function(req, res ) {
-    console.log(JSON.stringify(req.body))
-    
-    fs.open('testFile.txt', 'w', (err) => {
+async function OpenFileAs(param){
+    await fs.open(FileName, param, (err) => {
         if(err) throw err;
-        console.log('File created');
-        
-        fs.appendFile('testFile.txt', JSON.stringify(req.body), (err) => {
-            if(err) throw err;
-            console.log('Data has been added!');
-            res.send('response from post');
-        });
+//        console.log('File created');
     });
+}
+async function WriteFile(text){
+    OpenFileAs('w')
+    await fs.appendFile(FileName, text, (err) => {
+        if(err) throw err;
+        console.log('Data has been added!');
+    });
+}
+function ReadFile(){
+    OpenFileAs('r')
+    console.log("Синхронное чтение файла")
+    let fileContent = fs.readFileSync(FileName, "utf8");
+    return fileContent
+}
+router.post('/test', function(req, res ) {
+    WriteFile(JSON.stringify(req.body))
+    res.send('response from post');
 });
 
 router.get('/test', function(req, res ) {
-    console.log(JSON.stringify(req.body))
-    
-    fs.open('testFile.txt', 'r', (err) => {
-        if(err) throw err;
-        
-        console.log("Синхронное чтение файла")
-        let fileContent = fs.readFileSync("testFile.txt", "utf8");
-//        console.log(fileContent);
-        res.send(fileContent);
-    });
+    res.send(ReadFile());
 });
 
 module.exports = router;
